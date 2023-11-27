104fa32c61e7315ec99576846e2deca1d166e3ca
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-27 18:33:54 -0500

    sets up Index.jsx

---
+++ Pages/Index.jsx
@@ -0,0 +1,13 @@
+import Planets from "../Components/Planets";
+
+function Index() {
+  return (
+    <div className="Index">
+        <h2 className="underline">Cosmos Planet Database</h2>
+      <h2>Come See All of the Planets Humanity Has Discovered!</h2>
+      <Planets />
+    </div>
+  );
+}
+
+export default Index;
