c861b84926569f4a06cf2ce757799a6460238db3
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-27 18:35:10 -0500

    sets up New.jsx

---
+++ Pages/New.jsx
@@ -0,0 +1,14 @@
+import PlanetNewForm from "../Components/PlanetNewForm";
+
+
+
+function New() {
+  return (
+    <div className="New">
+      <h2 className="newPlanetHeader">Help Add to Our Database of Planets</h2>
+      <PlanetNewForm />
+    </div>
+  );
+}
+
+export default New;
