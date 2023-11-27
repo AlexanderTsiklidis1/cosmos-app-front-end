f89de116f8532b13bd0e9f8e5bf682985ad8d001
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-27 18:36:42 -0500

    sets up Edit.jsx

---
+++ Pages/Edit.jsx
@@ -0,0 +1,12 @@
+import PlanetEditForm from "../Components/PlanetEditForm";
+
+function Edit() {
+  return (
+    <div className="New Edit">
+      <h2 className="underline">Please Help Keep Our Planet Database up to Date</h2>
+      <PlanetEditForm />
+    </div>
+  );
+}
+
+export default Edit;
