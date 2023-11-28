4789dd5c5083beb2339b2865329a4fdeb3d847aa
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-28 02:54:26 -0500

    up to date

--- Components/PlanetDetails.jsx
+++ Components/PlanetDetails.jsx
@@ -28,3 +28,33 @@ function PlanetDetails() {
           })
           .catch((err) => console.error(err));
       };
+
+      return (
+        <article className="home-container">
+          
+          <img className="app-profile_pic" src = {post.profile_pic ? post.profile_pic: "src/assets/default_profile_pic.jpeg"} />
+          <h5 className="app-heading">
+            Post Message: {post.postmessage}
+          </h5>
+          <img className="app-post_pic" src = {post.post_pic ? post.post_pic: "src/assets/default_post_pic.jpeg"} />
+          <div className="showNavigation">
+            <div>
+              {" "}
+              <Link to={`/planets`}>
+                <button className="button">Back</button>
+              </Link>
+            </div>
+            <div>
+              {" "}
+              <Link to={`/planets/${index}/edit`}>
+                <button className="button" style={{ padding: "10px" }}>Edit</button>
+              </Link>
+            </div>
+            <div>
+              {" "}
+              <button onClick={handleDelete} className="button" style={{ padding: "10px" }}>Delete</button>
+            </div>
+          </div>
+        </article>
+      );
+    }
