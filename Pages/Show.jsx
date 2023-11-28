08434608b6beafec33b8bcfe417cc85d51ddb305
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-27 20:59:12 -0500

    builds show.jsx component

---
+++ Pages/Show.jsx
@@ -0,0 +1,12 @@
+import PlanetDetails from "../Components/PlanetDetails";
+
+function Show() {
+  return (
+    <div className="Show">
+      <h2 className="underline">Everything About Your Selected Planet</h2>
+      <PlanetDetails />
+    </div>
+  );
+}
+
+export default Show;
