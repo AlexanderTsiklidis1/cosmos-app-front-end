cf2f43e648e94ba328708b413ba22b48bd702468
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-27 21:01:41 -0500

    builds useEffect and useState for PlanetDetails component

--- Components/PlanetDetails.jsx
+++ Components/PlanetDetails.jsx
@@ -1,3 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 const API = import.meta.env.VITE_BASE_URL;
+
+function PlanetDetails() {
+    const [planet, setPlanet] = useState({});
+    const { index } = useParams();
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      fetch(`${API}/planets/${index}`)
+        .then((response) => response.json())
+        .then((fetchedPlanet) => {
+            console.log(fetchedPlanet)
+          setPost(fetchedPlanet);
+        })
+        .catch(() => navigate("/not-found"));
+    }, [index, navigate]);
