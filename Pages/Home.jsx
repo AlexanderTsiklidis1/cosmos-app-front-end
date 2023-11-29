22681ca59e8623a6ab3a6ebbc1c8f87c93d4f88d
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-29 17:27:04 -0500

    makes change in text to homepage

--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -7,11 +7,11 @@ function Home() {
       <h2 className="welcome-header">Welcome to the COSMOS App!</h2>
       <p className="description">Using our COSMOS App, you will be able to:</p>
       <ul className="feature-list">
-        <li className="feature-item">1 - Create a celestial object to update our database with information on a new celestial object -- Subject to Review</li>
+        <li className="feature-item">1 - Create a celestial object to update our database with information on a new celestial object -- Subject to Admin Review</li>
         <li className="feature-item">2 - See all of the celestial objectes in our database and obtain information on those celestial objects</li>
         <li className="feature-item">3 - Edit an existing celestial object</li>
         <li className="feature-item">4 - Delete an existing celestial object</li>
-        <li className="feature-item">5 - Go ahead and click on the celestial objects section to view our database of the Cosmos; or on New to create a new celestial object for our database -- Subject to Review </li>
+        <li className="feature-item">5 - Go ahead and click on the celestial objects section to view our database of the Cosmos; or on New to create a new celestial object for our database -- Subject to Admin Review </li>
       </ul>
       <p className="description">We do hope you enjoy our COSMOS app, and please tell your friends and spread the word!</p>
     </div>
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Cosmos-app-front-end
+# Alexander Tsiklids' COSMOS APP
+
+## Overview
+
+In the COSMOS APP a user will be able to read an index of celestial objects we have stored in our database, and obtain information on each individual celestial object stored in the database.
+
+Additionally, a user will be able to delete as well as update an existing celestial object if new information or discovery has been made regarding the cosmos. All deletes and edits will be subject to review by our admins for accuracy, until we build a more robust operating system.
+
+Lastly, a user will be able to create their own celestial object. This too is subject to admin review.
+
+The Cosmos app is comprised of two repositiories a front-end application that communicates with a back-end server to render our celestial objects  and reply data.
+
+
+## Getting Started
+
+Please use the deployed front-end website on netlify(https://timely-gingersnap-37dfa8.netlify.app) and back-end server on render() in order to begin using our app
+
+### Prerequisites
+
+As stated above, you will need to go live on both the front-end application and back-end server in order to make full use of teh COSMOS app
+
+## Contributing
+
+Alexander Tsiklidis
+
+## Acknowledgements
+
+Shout Out to all of the Pursuit 10.2 Nights and Weekends Cohort and instructional Team for getting through the personal-portfolio-project. Great job!
+
+---
+
+**Happy coding!**
