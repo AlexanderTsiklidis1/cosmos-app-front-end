7804c519186b42776af020df474c9dc64ea71fa0
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-29 07:30:29 -0500

    fixes a major styling issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './index.css'
+import './index.css';
 import Footer from '../Components/Footer';
 import Edit from '../Pages/Edit';
 import FourOFour from '../Pages/FourOFour';
@@ -9,31 +9,38 @@ import Index from '../Pages/Index';
 import New from '../Pages/New';
 import Show from '../Pages/Show';
 import NavBar from '../Components/NavBar';
+import backgroundImage from '/src/assets/Space-Background-Images.jpg';
 
 function App() {
+  const bodyStyles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  };
 
   return (
-    <div className="App">
     <Router>
-      <NavBar />
-      <div className="main-content">
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/planets" element={<Index />} />
-            <Route path="/planets/new" element={<New />} />
-            <Route path="/planets/:index" element={<Show />} />
-            <Route path="/planets/:index/edit" element={<Edit />} />
-            <Route path="*" element={<FourOFour />} />
-          </Routes>
-        </main>
-      
+      <div style={bodyStyles}>
+        <NavBar />
+        <div className="main-content" style={{ flex: 1 }}>
+          <main className="main">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/planets" element={<Index />} />
+              <Route path="/planets/new" element={<New />} />
+              <Route path="/planets/:index" element={<Show />} />
+              <Route path="/planets/:index/edit" element={<Edit />} />
+              <Route path="*" element={<FourOFour />} />
+            </Routes>
+          </main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </Router>
-  </div>
-);
-    
+  );
 }
 
-export default App
+export default App;
