8b184030cead3538bc1d3ede7d20a2367347fcd4
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-29 01:18:39 -0500

    styles navbar and homepage

--- Components/NavBar.jsx
+++ Components/NavBar.jsx
@@ -4,14 +4,14 @@ export default function NavBar() {
   return (
     <nav className="navbar">
       <div className="nav-links">
+        <Link to="/planets" className="nav-link">
+        Celestial Objects
+        </Link>
         <Link to="/" className="nav-link">
           Home
         </Link>
-        <Link to="/planets" className="nav-link">
-          Planets
-        </Link>
         <Link to="/planets/new" className="nav-link">
-          New Planet
+          New Celestial Object
         </Link>
       </div>
     </nav>
--- Pages/Home.jsx
+++ Pages/Home.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 function Home() {
   return (
     <div className="home-container">
-      <h2 className="welcome-header">Welcome</h2>
-      <h3 className="subtitle">To the COSMOS App!</h3>
+      <h2 className="welcome-header">Welcome to the COSMOS App!</h2>
       <p className="description">Using our COSMOS App, you will be able to:</p>
       <ul className="feature-list">
-        <ul className="feature-item">Create a Planet to update our database with information on a new planet -- Subject to Review</ul>
-        <ul className="feature-item">See all of the Planets in our database and obtain information on those planets</ul>
-        <ul className="feature-item">Edit an Existing Planet</ul>
-        <ul className="feature-item">Go ahead and click on the planets section to view our database of planets; or on New to create a new planet for our database(subject to review) </ul>
+        <li className="feature-item">1 - Create a celestial object to update our database with information on a new celestial object -- Subject to Review</li>
+        <li className="feature-item">2 - See all of the celestial objectes in our database and obtain information on those celestial objects</li>
+        <li className="feature-item">3 - Edit an existing celestial object</li>
+        <li className="feature-item">4 - Delete an existing celestial object</li>
+        <li className="feature-item">5 - Go ahead and click on the celestial objects section to view our database of the Cosmos; or on New to create a new celestial object for our database -- Subject to Review </li>
       </ul>
       <p className="description">We do hope you enjoy our COSMOS app, and please tell your friends and spread the word!</p>
     </div>
