e55be536071a14c2dc209c35ecea94e23220613b
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-11-29 00:42:24 -0500

    finishes stylesheet designation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './index.css'
 import Footer from '../Components/Footer';
 import Edit from '../Pages/Edit';
 import FourOFour from '../Pages/FourOFour';
